943fb3c8d30712e615cfef80dda3ff018b8d98c2
GodProgrammer512 <patezinhoverde@gmail.com>
2025-12-11 04:32:41 -0300

    Add file Main.c

---
+++ Main.c
@@ -0,0 +1,18 @@
+/* Include guard: */
+#ifndef _HW_C_
+#define _HW_C_
+
+/* Importations: */
+#include <stdio.h>
+
+
+/* Main code: */
+int main()
+{
+	puts("Hello, World!");
+
+	return 0;
+}
+
+/* End code: */
+#endif
